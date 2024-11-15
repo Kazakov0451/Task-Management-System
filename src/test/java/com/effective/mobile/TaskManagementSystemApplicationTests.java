7ad3c73dd4cbad6149add5dfa4c014c76ecf08af
Egorkka74 <Egorkka7401@gmail.com>
2024-11-15 13:52:48 +0500

    Init

---
+++ src/test/java/com/effective/mobile/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effective.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
